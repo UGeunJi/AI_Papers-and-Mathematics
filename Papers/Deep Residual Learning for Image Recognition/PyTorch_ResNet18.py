9ec72c506cff13cd69907440e6204eb55489ca92
UGeun Ji <84713532+UGeunJi@users.noreply.github.com>
2023-02-23 12:11:07 +0900

    Create PyTorch_ResNet18.py

---
+++ Papers/Deep Residual Learning for Image Recognition/PyTorch_ResNet18.py
@@ -0,0 +1,173 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import os
+
+# ResNet18을 위해 최대한 간단히 수정한 BasicBlock 클래스 정의
+class BasicBlock(nn.Module):
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+
+        # 3x3 필터를 사용 (너비와 높이를 줄일 때는 stride 값 조절)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes) # 배치 정규화(Batch normalization)
+
+        # 3x3 필터를 사용 (패딩을 1만큼 주기 때문에 너비와 높이가 동일)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes) # 배치 정규화(Batch normalization)
+
+        self.shortcut = nn.Sequential() # identity인 경우
+        if stride != 1: # stride가 1이 아니라면, Identity mapping이 아닌 경우
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes)
+            )
+    
+    def forward(self, x):
+      out = F.relu(self.bn1(self.conv1(x)))
+      out = self.bn2(self.conv2(out))
+      out += self.shortcut(x) # (핵심) skip connection
+      out = F.relu(out)
+      return out
+
+# ResNet 클래스 정의
+class ResNet(nn.Module):
+    def __init__(self, block, num_block, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        # 64개의 3x3 필터(filter)를 사용
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_block[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_block[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_block[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_block[3], stride=2)
+        self.linear = nn.Linear(512, num_classes)
+    
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes # 다음 레이어를 위해 채널 수 변경
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+# ResNet18 함수 정의
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+  
+import torchvision
+import torchvision.transforms as transforms
+
+transform_train = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform_train)
+test_dataset = torchvision.datasets.MNIST(root='/data', train=False, download=True, transform=transform_test)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=4)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=False, num_workers=4)
+
+device = 'cuda'
+
+net = ResNet18()
+net = net.to(device)
+net = torch.nn.DataParallel(net)
+cudnn.benchmark = True
+
+learning_rate = 0.01
+file_name = 'resnet18_mnist.pt'
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0002)
+
+def train(epoch):
+    print('\n[ Train epoch: %d ]' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+
+        benign_outputs = net(inputs)
+        loss = criterion(benign_outputs, targets)
+        loss.backward()
+
+        optimizer.step()
+        train_loss += loss.item()
+        _, predicted = benign_outputs.max(1)
+
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        if batch_idx % 100 == 0:
+            print('\nCurrent batch: ', str(batch_idx))
+            print('Current benign train accuracy: ', str(predicted.eq(targets).sum().item() / targets.size(0)))
+            print('Current benign train loss: ', loss.item())
+
+    print('\nTotal benign train accuracy: ', 100. * correct / total)
+    print('Total benign train loss: ', train_loss)
+
+
+def test(epoch):
+    print('\n[ Test epoch: %d ]' % epoch)
+    net.eval()
+    loss = 0
+    correct = 0
+    total = 0
+
+    for batch_idx, (inputs, targets) in enumerate(test_loader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        total += targets.size(0)
+
+        outputs = net(inputs)
+        loss += criterion(outputs, targets).item()
+
+        _, predicted = outputs.max(1)
+        correct += predicted.eq(targets).sum().item()
+
+    print('\nTest accuracy: ', 100 * correct / total)
+    print('Test average loss: ', loss / total)    
+
+    state = {
+        'net': net.state_dict()
+    }
+    if not os.path.isdir('checkpoint'):
+        os.mkdir('checkpoint')
+    torch.save(state, './checkpoint/' + file_name)
+    print('Model Saved!')
+
+
+def adjust_learning_rate(optimizer, epoch):
+    lr = learning_rate
+    if epoch >= 5:
+        lr /= 10
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+        
+for epoch in range(0, 10):
+    adjust_learning_rate(optimizer, epoch)
+    train(epoch)
+    test(epoch)
